0f2f18e82b8e4536d8f258008022bcea63e85540
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2023-12-08 00:47:58 +0000

    code corregido

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generarExcusa = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+  let whoAlAzar = who[Math.floor(Math.random() * who.length)];
+  let actionAlAzar = action[Math.floor(Math.random() * action.length)];
+  let whatAlAzar = what[Math.floor(Math.random() * what.length)];
+  let whenAlAzar = when[Math.floor(Math.random() * when.length)];
+  // // console.log("Hello Rigo from the console!");
+  let excusaFinal `${whoAlAzar} ${actionAlAzar} ${whatAlAzar} ${whenAlAzar}`;
+  return excusaFinal;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = document.getElementById('excuse');
+  excusa.innerHTML = generarExcusa();
 };
